1317ee22534db97a5ff77b9b4a6f585ef250d7ee
payatsu <6873447+payatsu@users.noreply.github.com>
2020-04-12 17:51:58 +0900

    refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,5 @@
 username=`tail -n 1 /etc/passwd | cut -d: -f1`
 [ "${USER}" = "${username}" ] || usermod -l ${USER} -d /home/${USER} -m ${username} || exit
 exec runuser -u ${USER} -- "$@"
+
+# vim: set expandtab :
