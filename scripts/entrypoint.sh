1d64469cc6433081bdfaba9e86386e297ce4f8cc
payatsu <6873447+payatsu@users.noreply.github.com>
2020-04-15 12:53:16 +0900

    add working dir, volumes config to docker-compose.yml

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,7 @@
 
 username=`tail -n 1 /etc/passwd | cut -d: -f1`
 [ "${USER}" = "${username}" ] || usermod -l ${USER} -d /home/${USER} -m ${username} || exit
+chown  ${USER}:${USER} . || exit
 exec runuser -u ${USER} -- "$@"
 
 # vim: set expandtab :
