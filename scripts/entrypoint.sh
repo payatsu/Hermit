57fbc5d5afdebb4fdfe818f52f05811c456140b4
payatsu <6873447+payatsu@users.noreply.github.com>
2020-04-21 00:05:11 +0900

    fix usermod problem in entrypoint.sh
    `usermod` fails if the directory specified by -d has existed.
    
    with this commit, -d is superceded by `cp -Tr`.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,26 @@
 #!/bin/bash -l
 
+# set trap that invokes new `bash` with root permission whenever an error occurs.
+# errors are expected to cause `exit`(see below).
+trap bash EXIT
+
+[ -n "${DEBUG_ENTRYPOINT}" ] && set -x
+
 username=`tail -n 1 /etc/passwd | cut -d: -f1`
-[ "${USER}" = "${username}" ] || usermod -l ${USER} -d /home/${USER} -m ${username} || exit
+if [ "${USER}" != "${username}" ]; then
+    usermod -l ${USER} ${username} || exit
+    cp -Tr /etc/skel /home/${USER} || exit
+fi
+
+groupname=`tail -n 1 /etc/group | cut -d: -f1`
+if [ "${USER}" != "${groupname}" ]; then
+    groupmod -n ${USER} ${groupname} || exit
+fi
+
 chown  ${USER}:${USER} . || exit
+
+# the `bash` process running this script never EXIT(`exit` successfully),
+# because it does `exec` here.
 exec runuser -u ${USER} -- "$@"
 
 # vim: set expandtab :
